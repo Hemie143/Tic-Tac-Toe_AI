1802b0dd1a161242b4643ce817c0496c567817a3
hemie143 <hemie143@gmail.com>
2020-06-20 23:23:00 +0200

    Stage 4

---
+++ Problems/Bittersweet occasion/task.py
@@ -0,0 +1,8 @@
+# work with the variable `child`
+# child = input()
+if issubclass(child, Drinks):
+    print('Drinks')
+elif issubclass(child, Pastry):
+    print('Pastry')
+elif issubclass(child, Sweets):
+    print('Sweets')
--- Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
@@ -62,9 +62,115 @@ class TicTacToe:
             self.state = 'O wins'
             self.end = True
 
+    @staticmethod
+    def count_symbol(line, symbol):
+        # returns 3, if 3 symbols
+        # returns 2, if 2 symbols and 3rd cell is free
+        # returns -1, if 2 symbols and 3rd cell is taken by opponent
+        # returns 0 or 1 in other cases
+        count = line.count(symbol)
+        if count == 3:
+            return 3
+        elif count == 2:
+            opposite_symbol = 'X' if symbol == 'O' else 'O'
+            if line.count(opposite_symbol) == 1:
+                return -1
+            else:
+                return 2
+        else:
+            return count
+        return
+
+    def check_row(self, row_number, symbol):
+        row = self.cells[row_number]
+        return self.count_symbol(row, symbol)
+
+    def check_column(self, col_number, symbol):
+        column = [r[col_number] for r in self.cells]
+        return self.count_symbol(column, symbol)
+
+    def check_diagonal(self, diagonal, symbol):
+        if diagonal == 1:
+            diag = [self.cells[i][i] for i in range(0, 3)]
+        elif diagonal == 2:
+            diag = [self.cells[i][2-i] for i in range(0, 3)]
+        return self.count_symbol(diag, symbol)
+
+    def fill_row(self, n, symbol, symbol_new):
+        for i in range(0, 3):
+            cell = self.cells[n][i]
+            if cell != symbol and cell == ' ':
+                self.cells[n][i] = symbol_new
+
+    def fill_column(self, n, symbol, symbol_new):
+        for i in range(0, 3):
+            cell = self.cells[i][n]
+            if cell != symbol and cell == ' ':
+                self.cells[i][n] = symbol_new
+
+    def fill_diagonal(self, n, symbol, symbol_new):
+        if n == 1:
+            for i in range(0, 3):
+                cell = self.cells[i][i]
+                if cell != symbol and cell == ' ':
+                    self.cells[i][i] = symbol_new
+        elif n == 2:
+            for i in range(0, 3):
+                cell = self.cells[i][2-i]
+                if cell != symbol and cell == ' ':
+                    self.cells[i][2-i] = symbol_new
+
     def play_computer_easy(self, player):
         print('Making move level "easy"')
         found_cell = False
+        while not found_cell:
+            x = randint(0, 2)
+            y = randint(0, 2)
+            if self.cells[x][y] == " ":
+                self.cells[x][y] = player.symbol
+                found_cell = True
+
+    def play_computer_medium(self, player):
+        print('Making move level "medium"')
+        found_cell = False
+        # Win in one move ?
+        for i in range(0, 3):
+            if self.check_row(i, player.symbol) == 2:
+                self.fill_row(i, player.symbol, player.symbol)
+                found_cell = True
+                break
+        if not found_cell:
+            for i in range(0, 3):
+                if self.check_column(i, player.symbol) == 2:
+                    self.fill_column(i, player.symbol, player.symbol)
+                    found_cell = True
+                    break
+        if not found_cell:
+            for i in range(1, 2):
+                if self.check_diagonal(i, player.symbol) == 2:
+                    self.fill_diagonal(i, player.symbol, player.symbol)
+                    found_cell = True
+                    break
+        # Check opponent win in one move
+        opposite_symbol = 'X' if player.symbol == 'O' else 'O'
+        if not found_cell:
+            for i in range(0, 3):
+                if self.check_row(i, player.symbol) == 2:
+                    self.fill_row(i, player.symbol, opposite_symbol)
+                    found_cell = True
+                    break
+        if not found_cell:
+            for i in range(0, 3):
+                if self.check_column(i, player.symbol) == 2:
+                    self.fill_column(i, player.symbol, opposite_symbol)
+                    found_cell = True
+                    break
+        if not found_cell:
+            for i in range(1, 2):
+                if self.check_diagonal(i, player.symbol) == 2:
+                    self.fill_diagonal(i, player.symbol, opposite_symbol)
+                    found_cell = True
+                    break
         while not found_cell:
             x = randint(0, 2)
             y = randint(0, 2)
@@ -96,6 +202,8 @@ class TicTacToe:
             for player in [self.playerX, self.playerO]:
                 if player.type == 'easy':
                     self.play_computer_easy(player)
+                if player.type == 'medium':
+                    self.play_computer_medium(player)
                 elif player.type == 'user':
                     self.play_user(player)
                 self.print_grid()
@@ -117,6 +225,7 @@ class TicTacToe:
                 self.play()
             else:
                 print('Bad parameters!')
+            # exit()
 
 
 game = TicTacToe()
